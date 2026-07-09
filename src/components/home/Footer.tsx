cc10e361e41eed8c3aa3a36bdfa9ac49463a1a2f
22130107 <22130107@st.hcmuaf.edu.vn>
2026-07-09 14:05:05 +0700

    hi

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -7,9 +7,8 @@ export function Footer() {
     <footer className="bg-white pt-4 pr-0 pb-4 pl-0 mt-auto w-full shrink-0 border-t border-neutral-200">
       <div className="ml-auto mr-auto w-full max-w-[1536px] pt-0 pr-4 pb-0 pl-4">
         <div className="grid gap-[32px]" style={{"gridTemplateColumns":"repeat(5, minmax(0px, 1fr))"}}>
-          <div className="items-center flex justify-start gap-3" style={{"gridArea":"1 / 1 / 2 / 2"}}>
+          <div className="items-center flex justify-start" style={{"gridArea":"1 / 1 / 2 / 2"}}>
             <Logo size="lg" />
-            <span className="font-extrabold text-xl text-pink tracking-wide whitespace-nowrap">CÂY TRE ĐAM MỸ</span>
           </div>
           <div className="grid gap-[16px]" style={{"gridTemplateColumns":"repeat(4, minmax(0px, 1fr))","gridArea":"1 / 2 / 2 / 6"}}>
             <div className="items-center flex justify-start" style={{"gridArea":"1 / 1 / 2 / 2"}}>
